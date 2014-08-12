9a248e747eb2530028e6eabbcbb2aaca1f5669ee
Thomas Björk <tb@bytecode.se>
2014-08-12 13:43:40 +0200

    Added functions to convert an image to canvas, store data in webStorage. Added animations to favicons

--- README.md
+++ README.md
@@ -1,3 +1,91 @@
 # JUtils
 
 JUtils is a javascript library for jQuery with loads of small useful utilities.
+
+## jQuery(selector).fn
+These functions is attached to jQuerys selector.
+
+### jQuery(selector).faviconAdd()
+Load an image and use it as a favicon in the browser.
+
+Loads images found by the jQuery-selector and converts them into a favicon which is inserted into the documents head. If multiple elements are found then it is up to the browser to select which favicon to use. (The browser might use the first available favicon or, most likely, the last defined icon. This is entirely up to the browser and its internal design.)
+
+### jQuery(selector).faviconAnimationAdd(options)
+Adds images that matches the given selector to the animation list for the favicon.
+
+If the options object has the property "start" set to true then the animation starts directly. The options property "delay" specifies the interval between each image. This is given in milliseconds.
+
+To change the delay just call jQuery.faviconAnimationStart(newDelay); where "newDelay" is the new interval to use.
+
+#### options.start
+Set to true to start the interval directly.
+
+If start is omitted or set to false then the animation can be started by calling jQuery.faviconAnimationStart();
+
+#### options.delay
+Sets the delay for the update interval. This time is specified in milliseconds. When the interval gets too low then the favicon will flicker and you will not get a proper result.
+
+### jQuery(selector1).imageToCanvas(selector2)
+Loads images found by the jQuery-selector (selector1) and converts them to canvas elements. These are attached to the object specified by the selector called selector2.
+
+## jQuery.fn
+These functions can be reached from the jQuery-object itself without specifying a selector.
+
+### jQuery.faviconAdd(url)
+This function loads an image to a internal canvas object and transform it to a png which is used as an favicon.
+
+Load an image from the specified URL and use it as an favicon.
+
+### jQuery.faviconAnimationAdd(url)
+Adds an image to the animationList to create an animated favicon.
+
+### jQuery.faviconAnimationStart(delay)
+Start looping the images in the animationList. The delay specified is the time between shifting images for the favicon.
+
+If the delay, which is measured in milliseconds, is to small then the favicon will start to flicker and you will not get the intended result.
+
+If this function is called a second time then it will only set the delay once again. This makes it possible to change the delay efter the animation has been started.
+
+### jQuery.faviconAnimationStop()
+Stop an ongoing favicon animation.
+
+### jQuery.imageToCanvas(url)
+Load an image from the specified URL and convert it into a canvas object. The returned object is an jQuery object containing the canvas object.
+
+### jQuery.storage
+
+#### jQuery.storage.getEngineName()
+Returns which engine is used. Currently there are three valid values (localStorage, sessionStorage, local).
+
+##### localStorage
+The internal localStorage is used. Any value set here will remain until removed or until it moves beyond the timeout.
+
+##### sessionStorage
+The internal sessionStorage is used. Any value here will remain until the session expires or until it moves beyond the timeout.
+
+##### internal
+The browser doesn't support any webStorage so the data is stored in an object as key-value pairs. Any value here will be lost when the page reloads or when it moves beyond the timeout.
+
+#### jQuery.storage.get(key)
+Gets the value for the specified key from storage.
+
+If the key is invalid than the function returns null.
+
+#### jQuery.storage.set(key, value, timeout)
+Set the key to value in the storage.
+
+The timeout specifies for how long the key is valid in the storage. This time is given in seconds. If the timeout is omitted then the timesout defaults to 3600 seconds.
+
+#### jQuery.storage.isset(key)
+Checks if the value for the given key is valid in storage.
+
+#### jQuery.storage.unset(key)
+Removes the value for the given key from storage.
+
+#### jQuery.storage.touch(key, timeout)
+Refreshes the the timeout for the given key with a new timeout defined in seconds by the argument named timeout. If this argument for the timeout is omitted then the default timeout of 3600 seconds is used.
+
+#### jQuery.storage.getValid(key)
+Returns the time left until the value for the given key gets invalid.
+
+If the key is missing or invalid then this function returns false.
--- utils.js
+++ utils.js
@@ -1,6 +1,7 @@
 (function(jQuery) {
   var faviconObject = {
     draw: function(url) {
+      var link = jQuery('<link></link>');
       jQuery('<img></img>')
         .attr('src', url)
         .on('load', function() {
@@ -9,29 +10,231 @@
           canvas.width = img.width;
           canvas.height = img.height;
           canvas.getContext("2d").drawImage(img, 0, 0);
-          var f = jQuery('<link></link>');
-          f.attr('href', canvas.toDataURL('image/png'));
-          f.attr('rel', 'shortcut icon');
-          f.attr('type', 'image/png');
-          f.appendTo('head');
+          link.attr('href', canvas.toDataURL('image/png'));
+          link.attr('rel', 'shortcut icon');
+          link.attr('type', 'image/png');
+          link.appendTo('head');
         });
+      return link;
     },
     remove: function() {
       jQuery('link[rel="icon"],link[rel="short icon"]').remove();
+    },
+    _animateList: [],
+    _animateCount: 0,
+    _animateIndex: 0,
+    _animateInterval: null,
+    animate: function() {
+      var idx = faviconObject._animateIndex;
+      faviconObject._animateIndex = (faviconObject._animateIndex + 1) % faviconObject._animateCount;
+      if(faviconObject._animateList[idx]) {
+        jQuery('link[rel="icon"],link[rel="short icon"]').remove();
+        var link = jQuery('<link></link>');
+        link.attr('href', faviconObject._animateList[idx]);
+        link.attr('rel', 'shortcut icon');
+        link.attr('type', 'image/png');
+        link.appendTo('head');
+      }
+    },
+    animateLoadURL: function(url, index) {
+      jQuery('<img></img>')
+        .attr('src', url)
+        .on('load', function() {
+          var canvas = document.createElement('canvas');
+          var img = this;
+          canvas.width = img.width;
+          canvas.height = img.height;
+          canvas.getContext("2d").drawImage(img, 0, 0);
+          faviconObject._animateList[index] = canvas.toDataURL('image/png');
+        });
+    },
+    addAnimation: function(url) {
+      faviconObject.animateLoadURL(url, faviconObject._animateCount);
+      faviconObject._animateCount++;
+    },
+    animationLength: function() {
+      return faviconObject._animateCount;
+    },
+    startAnimation: function(delay) {
+      if(faviconObject._animateInterval !== null) {
+        clearInterval(faviconObject._animateInterval);
+      }
+      faviconObject._animateInterval = setInterval(faviconObject.animate, delay);
+    },
+    stopAnimation: function() {
+      if(faviconObject._animateInterval !== null) {
+        clearInterval(faviconObject._animateInterval);
+      }
     }
   };
+  var storageEngine = (function() {
+    var storage = window.localStorage || window.sessionStorage || {
+      _engineName: 'internal',
+      _items: {},
+      getItem: function(key) {
+        if(this._items[key]) {
+          return this._items[key];
+        }
+        return null;
+      },
+      setItem: function(key, value) {
+        this._items[key] = value;
+      },
+      removeItem: function(key) {
+        this._items[key] = null;
+      }
+    };
+    return {
+      getEngineName: function() {
+        if(storage._engineName) {
+          return storage._engineName;
+        } else if(window.localStorage) {
+          return 'localStorage';
+        } else if(window.sessionStorage) {
+          return 'sessionStorage';
+        }
+      },
+      timestamp: function() {
+        if(Date.getTime) {
+          return Date.getTime();
+        } else {
+          return +(new Date());
+        }
+      },
+      get: function(key) {
+        var data = storage.getItem(key);
+        if(data !== null) {
+          var arr = JSON.parse(data);
+          if(arr) {
+            if(arr.timeout) {
+              if(arr.timeout >= this.timestamp()) {
+                return arr.data;
+              } else {
+                return null;
+              }
+            }
+          }
+        }
+        return data;
+      },
+      set: function(key, value, timeout) {
+        timeout = timeout || 3600;
+        timeout *= 1000;
+        var data = {
+          data: value,
+          timeout: (this.timestamp() + timeout)
+        }
+        return storage.setItem(key, JSON.stringify(data));
+      },
+      isset: function(key) {
+        var data = storage.getItem(key);
+        if(data !== null) {
+          var obj = JSON.parse(data);
+          if(obj && obj.timeout && (obj.timeout >= this.timestamp())) {
+            return true;
+          }
+        }
+        return false;
+      },
+      unset: function(key) {
+        return storage.removeItem(key);
+      },
+      touch: function(key, timeout) {
+        var data = storage.getItem(key);
+        if(data !== null) {
+          var obj = JSON.parse(data);
+          if(obj && obj.timeout && (obj.timeout >= this.timestamp())) {
+            return this.set(key, obj, timeout);
+          }
+        }
+        return false;
+      },
+      getValid: function(key) {
+        var data = storage.getItem(key);
+        if(data !== null) {
+          var obj = JSON.parse(data);
+          var timestamp = this.timestamp();
+          if(obj && obj.timeout && (obj.timeout >= timestamp)) {
+            return ((obj.timeout - timestamp) / 1000);
+          }
+        }
+        return false;
+      }
+    };
+  }());
   
   jQuery.extend({
-    addFavicon: function(url) {
+    faviconAdd: function(url) {
       faviconObject.remove();
       faviconObject.draw(url);
+    },
+    faviconAnimationAdd: function(url) {
+      faviconObject.addAnimation(url);
+    },
+    faviconAnimationStart: function(delay) {
+      faviconObject.startAnimation(delay);
+    },
+    faviconAnimationStop: function() {
+      faviconObject.stopAnimation();
+    },
+    imageToCanvas: function(url) {
+      var canvas = document.createElement("canvas");
+      var image = jQuery('<img></img>').attr('src', url).on('load', function() {
+        var img = image.get(0);
+        canvas.width = img.width;
+        canvas.height = img.height;
+        canvas.getContext("2d").drawImage(img, 0, 0);
+      });
+      return jQuery(canvas);
+    },
+    storage: {
+      getEngineName: function() {
+        return storageEngine.getEngineName();
+      },
+      get: function(key) {
+        return storageEngine.get(key);
+      },
+      set: function(key, value, timeout) {
+        return storageEngine.set(key, value, timeout);
+      },
+      isset: function(key) {
+        return storageEngine.isset(key);
+      },
+      unset: function(key) {
+        return storageEngine.unset(key);
+      },
+      touch: function(key, timeout) {
+        return storageEngine.touch(key, timeout);
+      },
+      getValid: function(key) {
+        return storageEngine.getValid(key);
+      }
     }
   });
   jQuery.fn.extend({
-    addFavicon: function() {
+    faviconAdd: function() {
       return this.each(function() {
         faviconObject.remove();
         faviconObject.draw(jQuery(this).attr("src"));
+      });
+    },
+    faviconAnimationAdd: function(options) {
+      options = options || {};
+      if(options.start === true) {
+        faviconObject.startAnimation(options.delay || 250);
+      }
+      return this.each(function() {
+        faviconObject.addAnimation(jQuery(this).attr("src"));
+      });
+    },
+    imageToCanvas: function(sel) {
+      return this.each(function() {
+        var canvas = document.createElement("canvas");
+        var image = this;
+        canvas.width = image.width;
+        canvas.height = image.height;
+        canvas.getContext("2d").drawImage(image, 0, 0);
+        return jQuery(canvas).appendTo(sel);
       });
     }
   });
